06bfef7dcb50a2a0864112acf74465378617e199
Ches Martin <ches@whiskeyandgrits.net>
2013-03-07 21:05:35 +0700

    Make a spec pending until we figure something out for auth

--- spec/integration/messages_spec.rb
+++ spec/integration/messages_spec.rb
@@ -6,6 +6,7 @@ describe AngellistApi::Client::Messages do
   let(:client) { AngellistApi::Client.new }
 
   it 'gets all messages for a user' do
+    pending 'an approach for VCR integration testing without exposing a key publicly'
     messages = client.get_messages
     messages.first.should have_key :thread_id
   end
