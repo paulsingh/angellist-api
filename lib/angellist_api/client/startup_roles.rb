5f7fa27272f395eae5465c1ae041ecabe2c937b3
Ches Martin <ches@whiskeyandgrits.net>
2013-03-25 17:37:09 -0400

    Make the new version of the startup_roles endpoint opt-in
    This would otherwise be a breaking change for our library users and
    necessitate a major version bump, plus we were preventing users from
    overriding it until they could get their code updated.

--- lib/angellist_api/client/startup_roles.rb
+++ lib/angellist_api/client/startup_roles.rb
@@ -4,12 +4,23 @@ module AngellistApi
     #
     # @see http://angel.co/api/spec/startup_roles
     module StartupRoles
-      # Given a user_id, returns the companies that User is tagged in. Given a startup_id, 
-      # returns either the users and companies tagged in the given Startup (if direction is  
-      # incoming) or the companies which the given Startup is tagged in (if direction is 
-      # outgoing). If neither parameter is given, the authenticated user is used. Possible 
-      # roles include founder, employee, past_investor, current_investor, advisor, incubator 
-      # and referrer. Roles are paginated and ordered by most recently declared first.
+      # @deprecated Given a startup_id, returns the users involved in that
+      #   startup. Given a user_id, returns the startups that user is involved
+      #   in. If neither parameter is given, the authenticated user is used.
+      #   Possible roles include founder, employee, past_investor,
+      #   current_investor, advisor, incubator and referrer. Roles are paginated
+      #   and ordered by most recently declared first.
+      #
+      # When called with a :v => 1 parameter:
+      #
+      # Given a user_id, returns the companies that User is tagged in. Given a
+      # startup_id, returns either the users and companies tagged in the given
+      # Startup (if direction is incoming) or the companies which the given
+      # Startup is tagged in (if direction is outgoing). If neither parameter
+      # is given, the authenticated user is used. Possible roles include
+      # founder, employee, past_investor, current_investor, advisor, incubator
+      # and referrer. Roles are paginated and ordered by most recently declared
+      # first.
       #
       # @requires_authentication Optional
       # @paginated Yes
@@ -19,6 +30,8 @@ module AngellistApi
       #   you want to view.
       # @option options [Integer] :startup_id  The startup whose user
       #   relationships you want to view.
+      # @option options [String] :role The specific role you'd like to filter
+      #   on, e.g. "founder", "past_investor", "advisor"
       # @option options [String] :direction  Only applies if startup_id is present. 
       #   Either incoming or outgoing. Defaults to incoming.
       # @option options [Integer] :page Specifies the page of results to
@@ -30,7 +43,6 @@ module AngellistApi
       # @example Get users involved in startup with ID 1234, and their roles.
       #   AngellistApi.get_startup_roles(:startup_id => 1234)
       def get_startup_roles(options={})
-        options.merge!(:v => 1)
         get("1/startup_roles", options)
       end
     end
--- spec/integration/startup_roles_spec.rb
+++ spec/integration/startup_roles_spec.rb
@@ -5,24 +5,12 @@ describe AngellistApi::Client::StartupRoles,
 
   let(:client) { AngellistApi::Client.new }
 
-  it "gets a startup's outgoing relationships" do
+  it "gets a startup's relationships" do
     roles = client.get_startup_roles(:startup_id => 1124)
     roles.startup_roles.each do |relationship|
       ROLES.should include relationship.role
     end
-    roles.startup_roles.first.should have_key :tagged
-    roles.startup_roles.first.tagged.should have_key :type
-    roles.startup_roles.first.tagged.type.should eq "User"
-  end
-
-  it "gets a startup's incoming relationships" do
-    roles = client.get_startup_roles(:startup_id => 1124, :direction => "incoming")
-    roles.startup_roles.each do |relationship|
-      ROLES.should include relationship.role
-    end
-    roles.startup_roles.first.should have_key :tagged
-    roles.startup_roles.first.tagged.should have_key :type
-    roles.startup_roles.first.tagged.type.should eq "User"
+    roles.startup_roles.first.should have_key :user
   end
 
   it "gets a user's relationships" do
@@ -31,7 +19,42 @@ describe AngellistApi::Client::StartupRoles,
       ROLES.should include relationship.role
     end
     roles.startup_roles.first.should have_key :startup
-    roles.startup_roles.first.should have_key :tagged
+  end
+
+  context 'when opting into the v1 version of the resource' do
+    let(:base_params) { { :v => 1 } }
+
+    it "gets a startup's outgoing relationships" do
+      params = { :startup_id => 1124 }.merge(base_params)
+      roles = client.get_startup_roles(params)
+      roles.startup_roles.each do |relationship|
+        ROLES.should include relationship.role
+      end
+      roles.startup_roles.first.should have_key :tagged
+      roles.startup_roles.first.tagged.should have_key :type
+      roles.startup_roles.first.tagged.type.should eq "User"
+    end
+
+    it "gets a startup's incoming relationships" do
+      params = { :startup_id => 1124, :direction => 'incoming' }.merge(base_params)
+      roles = client.get_startup_roles(params)
+      roles.startup_roles.each do |relationship|
+        ROLES.should include relationship.role
+      end
+      roles.startup_roles.first.should have_key :tagged
+      roles.startup_roles.first.tagged.should have_key :type
+      roles.startup_roles.first.tagged.type.should eq "User"
+    end
+
+    it "gets a user's relationships" do
+      params = { :user_id => 2850 }.merge(base_params)
+      roles = client.get_startup_roles(params)
+      roles.startup_roles.each do |relationship|
+        ROLES.should include relationship.role
+      end
+      roles.startup_roles.first.should have_key :startup
+      roles.startup_roles.first.should have_key :tagged
+    end
   end
 end
 
